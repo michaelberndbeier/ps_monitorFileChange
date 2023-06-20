45c4edf0c44df05a63300907ed3d731509ad3019
mbb <code@michaelberndbeier.com>
2023-06-20 13:52:39 +0200

    Initial Commit

---
+++ get-folderItemChanges.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $pathToMonitor
+)
+
+$fileSystemWatcher = New-Object System.IO.FileSystemWatcher
+$fileSystemWatcher.IncludeSubdirectories = $true
+$fileSystemWatcher.Path = $pathToMonitor
+$fileSystemWatcher.EnableRaisingEvents = $true
+
+$fileWatcherAction =
+{
+    $changeDate = $(get-date)
+    $fullPath = $event.SourceEventArgs.FullPath
+    $changeType = $event.SourceEventArgs.ChangeType
+    $fileHash = get-filehash $fullPath | select-object -ExpandProperty Hash
+    echo "$fullPath;$changeType;$changeDate;$fileHash"
+}
+
+Register-ObjectEvent $fileSystemWatcher 'Created' -Action $fileWatcherAction
